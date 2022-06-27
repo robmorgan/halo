d1a3b8dedd10c1360b46f05e14b9d04561a307cd
Rob Morgan <robbym@gmail.com>
2022-06-27 20:59:50 +0800

    get 4 pars running on two different effects

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/fogleman/ease"
 	"github.com/nickysemenza/gola"
 	"github.com/robmorgan/halo/cuelist"
 	"github.com/robmorgan/halo/effect"
@@ -43,9 +44,15 @@ func main() {
 		panic("error creating config")
 	}
 
+	log.Println("Init fixtures")
+
 	// initialize all fixtures
 	fg := fixture.NewGroup()
-	par1 := fixture.NewFixture(1, 138, 8, map[int]fixture.Channel{
+
+	// left middle par
+	// For some reason the DMX address we are using at the moment is really -1 to what the actual
+	// fixture address is
+	par1 := fixture.NewFixture(1, 114, 8, map[int]*fixture.Channel{
 		1: {
 			Type:       fixture.TypeIntensity,
 			Address:    1,
@@ -58,6 +65,52 @@ func main() {
 		},
 	})
 	fg.AddFixture("left_middle_par", par1)
+
+	// right middle par
+	par2 := fixture.NewFixture(2, 138, 8, map[int]*fixture.Channel{
+		1: {
+			Type:       fixture.TypeIntensity,
+			Address:    1,
+			Resolution: 1,
+		},
+		2: {
+			Type:       fixture.TypeColorRed,
+			Address:    2,
+			Resolution: 1,
+		},
+	})
+	fg.AddFixture("right_middle_par", par2)
+
+	// left uplight par (A.123 -> 122)
+	par3 := fixture.NewFixture(3, 122, 8, map[int]*fixture.Channel{
+		1: {
+			Type:       fixture.TypeIntensity,
+			Address:    1,
+			Resolution: 1,
+		},
+		2: {
+			Type:       fixture.TypeColorRed,
+			Address:    2,
+			Resolution: 1,
+		},
+	})
+	fg.AddFixture("left_uplight_par", par3)
+
+	// right uplight par (A.131 -> 130)
+	par4 := fixture.NewFixture(3, 130, 8, map[int]*fixture.Channel{
+		1: {
+			Type:       fixture.TypeIntensity,
+			Address:    1,
+			Resolution: 1,
+		},
+		2: {
+			Type:       fixture.TypeColorRed,
+			Address:    2,
+			Resolution: 1,
+		},
+	})
+	fg.AddFixture("right_uplight_par", par4)
+
 	config.PatchedFixtures = fg
 
 	// Prepare some sequences
@@ -79,21 +132,23 @@ func main() {
 		// TODO - add group effects (like random PAR sparkle)
 	})
 
-	par, err := config.PatchedFixtures.GetFixture("left_middle_par")
-	if err != nil {
-		panic(fmt.Sprintf("could not get fixture: %s", err))
-	}
-	par.SetIntensity(1.0)
-	par.SetColor(0.0)
+	// Cheat a bit by setting intensity
+	par1.SetIntensity(1.0)
+	par2.SetIntensity(1.0)
+	par3.SetIntensity(1.0)
+	par4.SetIntensity(1.0)
 
 	// start render loop
+	log.Println("Starting render loop")
+
+	// lets make a color pulsing effect
+	fx1 := effect.NewEffect(ease.InQuart, 3.0, 0.2)
+	fx2 := effect.NewEffect(ease.OutCubic, 3.0, 1)
+
 	//tick := time.Tick(40 * time.Millisecond)
 	gl := engine.New(40*time.Millisecond, func(delta float64) {
-		log.Println("tick:", delta)
+		//log.Println(fmt.Printf("tick: %.7f", delta))
 		values := make([]byte, 512, 512)
-
-		//t := ease.InQuart(float64(i) / 255)
-		//	dVal := int(t * 255)
 
 		//values[119] = 255
 
@@ -103,21 +158,53 @@ func main() {
 		//values[141] = byte(dVal)
 		//values[141] = 255
 
-		par, err := config.PatchedFixtures.GetFixture("left_middle_par")
+		par1, err := config.PatchedFixtures.GetFixture("right_middle_par")
 		if err != nil {
 			panic(fmt.Sprintf("could not get fixture: %s", err))
 		}
 
-		// lets make a color pulsing effect
-		target := 1.0
-		effect := effect.NewEffect("InQuart", 1000)
+		par2, err := config.PatchedFixtures.GetFixture("left_middle_par")
+		if err != nil {
+			panic(fmt.Sprintf("could not get fixture: %s", err))
+		}
+
+		par3, err := config.PatchedFixtures.GetFixture("left_uplight_par")
+		if err != nil {
+			panic(fmt.Sprintf("could not get fixture: %s", err))
+		}
+
+		par4, err := config.PatchedFixtures.GetFixture("right_uplight_par")
+		if err != nil {
+			panic(fmt.Sprintf("could not get fixture: %s", err))
+		}
 
 		// TODO - do better error handling
-		parColor, _ := par.GetColor()
-		log.Println(fmt.Sprintf("PAR val: %.2f", parColor))
-		newColorVal := effect.Update(delta, target)
-		log.Println(fmt.Sprintf("calc val: %.2f", newColorVal))
-		par.SetColor(newColorVal)
+		parColor, err := par1.GetColor()
+		if err != nil {
+			panic(fmt.Sprintf("could not get color: %s", err))
+		}
+
+		newColorVal := fx1.Update(delta, float64(parColor))
+		log.Println(fmt.Sprintf("PAR oldVal=%.7f newVal=%.7f", parColor, newColorVal))
+		par1.SetColor(newColorVal)
+		par2.SetColor(newColorVal)
+		if err != nil {
+			panic(fmt.Sprintf("could not set color: %s", err))
+		}
+
+		// update uplighting pars
+		parColor2, err := par3.GetColor()
+		if err != nil {
+			panic(fmt.Sprintf("could not get color: %s", err))
+		}
+
+		newColorVal2 := fx2.Update(delta, float64(parColor2))
+		log.Println(fmt.Sprintf("PAR3 oldVal=%.7f newVal=%.7f", parColor2, newColorVal2))
+		par3.SetColor(newColorVal2)
+		par4.SetColor(newColorVal2)
+		if err != nil {
+			panic(fmt.Sprintf("could not set color: %s", err))
+		}
 
 		//t := ease.InQuart(float64(i) / 255)
 		//	dVal := int(t * 255)
@@ -128,16 +215,21 @@ func main() {
 				fmt.Printf("Fixture (%s) needs an update: %v\n", idx, fixture)
 
 				// prepare DMX packet
-				sendColor, _ := par.GetColor()
-				values[fixture.Address+1] = byte(uint8(sendColor) * 255)
+				sendColor, _ := fixture.GetColor()
+				values[fixture.Address+1] = byte(uint8(sendColor * 255))
+				values[fixture.Address] = 255 // let intensity be full
+
+				// we've updated
+				fixture.HasUpdated()
 			}
 		}
 
 		if status, err := client.SendDmx(1, values); err != nil {
-			log.Printf("SendDmx: 1: %v", err)
-		} else {
-			log.Printf("SendDmx: 1: %v", status)
-		}
+			log.Printf("Error Sending Dmx: %v", status)
+		} //else {
+		// We are okay
+		//log.Printf("Error Sending Dmx: %v", status)
+		//}
 
 		// // get DMX on universe 1
 		// if x, err := client.GetDmx(1); err != nil {
@@ -145,6 +237,10 @@ func main() {
 		// } else {
 		// 	log.Printf("GetDmx: 1: %v", x.Data)
 		// }
+
+		// TODO - we are currently sleeping because olad complains that there is "No buffer space available".
+		// Somehow we'll need to make sure our updates are "real-time" enough, but don't overwhelm the process.
+		time.Sleep(10 * time.Millisecond)
 	})
 
 	gl.Start()
@@ -162,6 +258,7 @@ func main() {
 	// PAR 115
 	// PAR 139
 	// Don't stop main goroutine
+	log.Printf("Going into terminal loop")
 	for {
 	}
 }
