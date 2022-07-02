a6a7c3f254201d10f8aaeb1e09795e44ee6c3054
Rob Morgan <robbym@gmail.com>
2022-07-02 23:45:58 +0800

    add the shehds led bar beam profile

--- config/config.go
+++ config/config.go
@@ -38,12 +38,27 @@ func NewHaloConfig() (HaloConfig, error) {
 func initializeFixtureProfiles() map[string]profile.Profile {
 	out := map[string]profile.Profile{
 		"shehds-par": {
-			Name: "Shehds PAR",
+			Name: "Shehds LED Flat PAR 12x3W RGBW",
 			Channels: map[string]int{
 				profile.ChannelTypeIntensity: 1,
 				profile.ChannelTypeRed:       2,
 				profile.ChannelTypeGreen:     3,
 				profile.ChannelTypeBlue:      4,
+			},
+		},
+		"shehds-led-bar-beam-8x12w": {
+			Name: "Shehds LED Bar Beam 8x12W RGBW",
+			// 9 channel mode
+			Channels: map[string]int{
+				profile.ChannelTypeMotorPosition:  1,
+				profile.ChannelTypeMotorSpeed:     2,
+				profile.ChannelTypeFunctionSelect: 3,
+				profile.ChannelTypeFunctionSpeed:  4,
+				profile.ChannelTypeIntensity:      5,
+				profile.ChannelTypeRed:            6,
+				profile.ChannelTypeGreen:          7,
+				profile.ChannelTypeBlue:           8,
+				profile.ChannelTypeWhite:          9,
 			},
 		},
 	}
--- config/patch.go
+++ config/patch.go
@@ -13,6 +13,7 @@ func PatchFixtures() []PatchedFixture {
 
 	s = append(s, patchFrontPars()...)
 	s = append(s, patchUplightPars()...)
+	s = append(s, patchBeamBars()...)
 
 	return s
 }
@@ -51,6 +52,23 @@ func patchUplightPars() []PatchedFixture {
 			Address:  130,
 			Universe: 1,
 			Profile:  "shehds-par",
+		},
+	}
+}
+
+func patchBeamBars() []PatchedFixture {
+	return []PatchedFixture{
+		PatchedFixture{
+			Name:     "left_beam_bar",
+			Address:  163,
+			Universe: 1,
+			Profile:  "shehds-led-bar-beam-8x12w",
+		},
+		PatchedFixture{
+			Name:     "right_beam_bar",
+			Address:  57,
+			Universe: 1,
+			Profile:  "shehds-led-bar-beam-8x12w",
 		},
 	}
 }
