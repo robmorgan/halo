d6eb78fbec154e6ceb0f9c7630df19fe6e3a22cb
Rob Morgan <robbym@gmail.com>
2024-07-24 08:15:16 +0800

    add artnet

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,6 +3,10 @@ use std::io::{stdout, Write};
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
+use artnet_protocol::*;
+use std::net::{ToSocketAddrs, UdpSocket};
+mod artnet;
+
 //const CHARS: [&str; 4] = ["|", "/", "-", "\\"];
 
 fn main() {
@@ -20,10 +24,24 @@ fn main() {
 
     // Due to Windows timers having a default resolution 0f 15.6ms, we need to use a "too high"
     // value to achieve ~60Hz
-    let period = Duration::from_micros(1000000 / 120);
+    //let period = Duration::from_micros(1000000 / 120);
+    let period = Duration::from_millis(500);
     let mut last_instant = Instant::now();
 
     let mut stdout = stdout();
+
+    let art_net_controller = artnet::ArtNet::new().unwrap();
+    //art_net_controller.s
+
+    let socket = UdpSocket::bind(("0.0.0.0", 6455)).unwrap();
+    let broadcast_addr = ("255.255.255.255", 6454)
+        .to_socket_addrs()
+        .unwrap()
+        .next()
+        .unwrap();
+    socket.set_broadcast(true).unwrap();
+    let buff = ArtCommand::Poll(Poll::default()).write_to_buffer().unwrap();
+    socket.send_to(&buff, &broadcast_addr).unwrap();
 
     loop {
         let delta = Instant::now() - last_instant; // Is this timer accurate enough?
@@ -33,14 +51,45 @@ fn main() {
 
         link.set_tempo_callback(|tempo| println!("Tempo: {}", tempo));
 
+        //art_net_controller.send_data(vec![0xff; 512]);
+
+        println!("Calling socket.recv_from");
+        let mut buffer = [0u8; 1024];
+        let (length, addr) = socket.recv_from(&mut buffer).unwrap();
+        let command = ArtCommand::from_buffer(&buffer[..length]).unwrap();
+
+        println!("Received {:?}", command);
+        match command {
+            ArtCommand::Poll(poll) => {
+                // This will most likely be our own poll request, as this is broadcast to all devices on the network
+                println!("Recv poll {:?}", poll);
+            }
+            ArtCommand::PollReply(reply) => {
+                // This is an ArtNet node on the network. We can send commands to it like this:
+                //art_net_controller.send_data(vec![0xff; 512]);
+
+                let command = ArtCommand::Output(Output {
+                    // length: dmx.len() as u16,
+                    //data: dmx.into(),
+                    port_address: 1.into(),
+                    data: vec![0xff; 512].into(),
+                    ..Output::default()
+                });
+
+                let bytes = command.write_to_buffer().unwrap();
+                socket.send_to(&bytes, &addr).unwrap();
+            }
+            _ => {}
+        }
+
         print!(
             "\rRunning Frq: {: >3}Hz    Peers:{}   BPM: {}    ",
             1000000 / (delta.as_micros().max(1)),
             link.num_peers(),
             state.tempo()
         );
-        stdout.flush().unwrap();
 
+        stdout.flush().unwrap();
         sleep(period);
     }
 }
